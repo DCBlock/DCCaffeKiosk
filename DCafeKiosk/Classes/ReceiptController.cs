f8c3a88c4802742cd49d22052b0d059b434387b7
Jung Byung Ok <jbo2541@gmail.com>
2019-01-31 09:56:47 +0900

    #12 영수증 하단 문구 수정

--- DCafeKiosk/Classes/ReceiptController.cs
+++ DCafeKiosk/Classes/ReceiptController.cs
@@ -172,10 +172,10 @@ namespace DCafeKiosk
             BXLAPI.LineFeed(1);
 
             //하단 출력
-            string strFooter1 = "결제 취소시 반드시 영수증을 카운터에 제출해 주시기 바랍니다. 결제 취소는 30분 이내에 요청해야 합니다.";
+            string strFooter1 = "결제 취소시 반드시 영수증을 카운터에 제출해 주시기 바랍니다.";
             string strFooter2 = "결제 취소는 30분 이내에 요청해야 합니다.";
-            BXLAPI.PrintText(strFooter1 + "\n", BXLAPI.BXL_ALIGNMENT_CENTER, BXLAPI.BXL_FT_DEFAULT, BXLAPI.BXL_TS_0WIDTH | BXLAPI.BXL_TS_0HEIGHT);
-            BXLAPI.PrintText(strFooter2 + "\n", BXLAPI.BXL_ALIGNMENT_CENTER, BXLAPI.BXL_FT_DEFAULT, BXLAPI.BXL_TS_0WIDTH | BXLAPI.BXL_TS_0HEIGHT);
+            BXLAPI.PrintText(strFooter1 + "\n", BXLAPI.BXL_ALIGNMENT_LEFT, BXLAPI.BXL_FT_DEFAULT, BXLAPI.BXL_TS_0WIDTH | BXLAPI.BXL_TS_0HEIGHT);
+            BXLAPI.PrintText(strFooter2 + "\n", BXLAPI.BXL_ALIGNMENT_LEFT, BXLAPI.BXL_FT_DEFAULT, BXLAPI.BXL_TS_0WIDTH | BXLAPI.BXL_TS_0HEIGHT);
             //--------------------------------------------------------------------
 
             BXLAPI.CutPaper();
